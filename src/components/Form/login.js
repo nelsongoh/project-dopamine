c52c6027bbde49deda09cfd9916b4392b55ea42a
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-13 12:52:28 +0800

    Wrote the Login form. Still WIP, needs to utilise the Firebase utils.

--- src/components/Form/login.js
+++ src/components/Form/login.js
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import useStyles from './loginStyles';
 import Link from '../Link';
+import Content from '../../lang/index';
 
 const LoginForm = () => {
   const classes = useStyles();
+  const [userCred, setUserCred] = useState({
+    email: "",
+    pwd: "",
+  })
 
   return (
     <Paper variant="outlined" className={classes.form}>
       <form className={classes.root}>
-        <TextField label="Email address" />
-        <TextField label="Password" />
+        <TextField label={Content('en').login.form.email.label} type="email" value={userCred.email} onChange={(e) => { setUserCred({...userCred, email: e.target.value }) }} />
+        <TextField label={Content('en').login.form.pwd.label} type="password" value={userCred.pwd} onChange={(e) => { setUserCred({...userCred, pwd: e.target.value }) }} />
       </form>
       <Button variant="contained" className={classes.btn} href="/dashboard" component={Link}>
         Login
