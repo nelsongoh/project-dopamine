d6f0bd6250d733b4b01bf62cb840915ffeab2596
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-14 16:40:28 +0800

    Added in the logic to include a spinner and relevant UI elements such as the text field and buttons including the states to maintain the email and password.

--- src/components/Form/login.js
+++ src/components/Form/login.js
@@ -1,25 +1,73 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import useStyles from './loginStyles';
-import Link from '../Link';
 import Content from '../../lang/index';
+import SignInUser from '../Auth/FirebaseAuth';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { User, LoginErrors } from '../../models/login';
+import Router from 'next/router';
+import UserContext from '../../contexts/user';
 
 const LoginForm = () => {
   const classes = useStyles();
-  const [userCred, setUserCred] = useState({
-    email: "",
-    pwd: "",
-  })
+  // Need to add in the error message state
+  const [userCred, setUserCred] = useState(User());
+  const [errors, setErrors] = useState(LoginErrors());
+  const [loginDisabled, setLoginDisabled] = useState(false);
+  const [spinner, setSpinner] = useState(null);
+  const user = useContext(UserContext);
+
+  useEffect(() => {
+    if (user !== null) {
+      Router.push("/dashboard");
+    }
+  }, [user])
+
+  // Function to log the user in, triggers the spinner while waiting for the sign-in to complete
+  const loginUser = async () => {
+    // Start the spinner
+    setSpinner(<CircularProgress />);
+    // Disable the login button
+    setLoginDisabled(true);
+    // Sign the user in
+    const signInOutcome = await SignInUser(userCred.email, userCred.pwd);
+    // End the spinner
+    setSpinner(null);
+    // Enable the login button again
+    setLoginDisabled(false);
+    // Set the error messages (if any)
+    setErrors(signInOutcome.error);
+  }
 
   return (
     <Paper variant="outlined" className={classes.form}>
+      <div className={classes.spinner}>
+        {spinner}
+      </div>
       <form className={classes.root}>
-        <TextField label={Content('en').login.form.email.label} type="email" value={userCred.email} onChange={(e) => { setUserCred({...userCred, email: e.target.value }) }} />
-        <TextField label={Content('en').login.form.pwd.label} type="password" value={userCred.pwd} onChange={(e) => { setUserCred({...userCred, pwd: e.target.value }) }} />
+        <TextField 
+          label={Content('en').login.form.email.label} 
+          type="email" value={userCred.email} 
+          onChange={(e) => { setUserCred({...userCred, email: e.target.value }) }} 
+          error={errors.email.hasError}
+          helperText={errors.email.msg}
+        />
+        <TextField 
+          label={Content('en').login.form.pwd.label}
+          type="password" value={userCred.pwd}
+          onChange={(e) => { setUserCred({...userCred, pwd: e.target.value }) }} 
+          error={errors.pwd.hasError}
+          helperText={errors.pwd.msg}
+        />
       </form>
-      <Button variant="contained" className={classes.btn} href="/dashboard" component={Link}>
+      <Button
+        variant="contained"
+        className={classes.btn}
+        onClick={() => { loginUser(); }}
+        disabled={loginDisabled}
+      >
         Login
       </Button>
     </Paper>
