c96cae32e6c1483532a3adbd70f4991020d42d2f
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-14 16:33:25 +0800

    Removed the 'login' field in the signInOutcome object. The state of the user object is captured by the Firebase observer and passed throughout the application in the React context.

--- src/components/Auth/FirebaseAuth.js
+++ src/components/Auth/FirebaseAuth.js
@@ -1,52 +1,42 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import initFirebase from '../../../utils/auth/initFirebase';
+import { LoginErrors } from '../../models/login';
 
 // Initialize the Firebase app
 initFirebase();
 
 const signInUserEmailPwd = async (email, pwd) => {
   let signInOutcome = {
-    login: true,
-    error: null,
+    error: LoginErrors(),
   }
 
-  await firebase.auth().signInWithEmailAndPassword(email, pwd)
+  firebase.auth().signInWithEmailAndPassword(email, pwd)
     .catch((error) => {
       switch (error.code) {
         case 'auth/invalid-email':
-          signInOutcome.login = false;
-          signInOutcome.error = {
-            email: "Invalid email address."
-          }
+          signInOutcome.error.email.hasError = true;
+          signInOutcome.error.email.msg = "Invalid email address.";
           break;
 
         case 'auth/user-disabled':
-          signInOutcome.login = false;
-          signInOutcome.error = {
-            email: "This account has been disabled. Please contact an administrator."
-          }
+          signInOutcome.error.email.hasError = true;
+          signInOutcome.error.email.msg = "This account has been disabled. Please contact an administrator.";
           break;
 
         case 'auth/user-not-found':
-          signInOutcome.login = false;
-          signInOutcome.error = {
-            email: "This user does not exist."
-          }
+          signInOutcome.error.email.hasError = true;
+          signInOutcome.error.email.msg = "This user does not exist.";
           break;
 
         case 'auth/wrong-password':
-          signInOutcome.login = false;
-          signInOutcome.error = {
-            pwd: "Invalid password."
-          }
+          signInOutcome.error.pwd.hasError = true;
+          signInOutcome.error.pwd.msg = "Invalid password.";
           break;
 
         default:
-          signInOutcome.login = false;
-          signInOutcome.error = {
-            email: "Something has gone wrong with the sign-in process. Please try again later."
-          }
+          signInOutcome.error.email.hasError = true;
+          signInOutcome.error.email.msg = "Something has gone wrong with the sign-in process. Please try again later.";
           break;
       }
     });
