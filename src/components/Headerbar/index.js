24a432fb8ff6643a9995ce20b8ce93d758af8ca8
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-17 18:16:43 +0800

    Added the Login widget to the Headerbar and the relevant styles.

--- src/components/Headerbar/index.js
+++ src/components/Headerbar/index.js
@@ -2,13 +2,17 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Content from '../../lang';
+import useStyles from './headerbarStyles';
+import LoginWidget from './Widgets/Login';
 
 const Headerbar = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <AppBar>
         <Toolbar position="static">
-          <Typography variant="h4">{Content('en').title}</Typography>
+          <Typography variant="h4" className={classes.title}>{Content('en').title}</Typography>
+          <LoginWidget />
         </Toolbar>
       </AppBar>
       <Toolbar />
