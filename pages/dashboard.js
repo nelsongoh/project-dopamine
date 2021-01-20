532be470a1efab63ed47da7f2d229be0d384623c
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-20 17:43:36 +0800

    Included the API call to retrieve the dashboard content. Displays a Loading screen while waiting for the API call to complete. Also included a Sidebar for the Dashboard page.

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Headerbar from '../src/components/Headerbar';
 import PageAuth from '../src/components/Auth/PageAuth';
+import UserContext from '../src/contexts/user';
+import useDashboardContent from '../lib/useDashboardContent';
+import LoadingScreen from '../src/components/Loading/loading';
+import Sidebar from '../src/components/Sidebar';
 
 const Dashboard = () => {
+  // Fetch the user's view of the dashboard
+  const user = useContext(UserContext);
+  const { content, isLoading, isError } = useDashboardContent(user.uid);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  if (isError) {
+    console.log("There's an error.");
+  }
+  
   return (
     <div>
-      <Headerbar />
+      <Headerbar isTopStack={true} />
+      <Sidebar content={content.dashboard.views} />
       <h1>INSIDE THE DASHBOARD</h1>
     </div>
   );
@@ -15,6 +32,6 @@ const AuthDashboard = () => {
   return (
     <PageAuth ProtectedComponent={Dashboard} />
   )
-}
+};
 
 export default AuthDashboard;
