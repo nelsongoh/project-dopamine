45bd7555019361c404c8fc79dfc7f19c9d5d730e
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-28 13:05:58 +0800

    Created a component to dynamically display the lucky colors for the given month.

---
+++ src/components/LuckyColors/index.js
@@ -0,0 +1,39 @@
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import Paper from '@material-ui/core/Paper';
+import Content from '../../lang';
+import useStyles from './luckyColorsStyles';
+
+const LuckyColors = ({ currDate, content }) => {
+  const classes = useStyles();
+  const currMthColors = content[currDate.getMonth()];
+
+  return (
+    <Grid container spacing={3}>
+      <Grid container item justify="center">
+        <Typography variant="h3">{Content('en').pages.calendar.luckyColors.title}</Typography>
+      </Grid>
+      <Grid container item justify="space-evenly">
+        <GridList cellHeight={180} className={classes.gridList} cols={4}>
+          {Object.keys(currMthColors).map((color) => {
+            if (currMthColors[color] !== false) {
+              return (
+                <GridListTile key={Math.random()}>
+                  <Paper variant="outlined" className={classes[color]} />
+                  <GridListTileBar
+                    title={typeof(currMthColors[color]) == 'string' ? `(${currMthColors[color]}) ${color}` : color}
+                  />
+                </GridListTile>
+              );
+            }
+          })}
+        </GridList>
+      </Grid>
+    </Grid>
+  )
+};
+
+export default LuckyColors;
