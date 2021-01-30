18e13b205a7fc77047a2335d0952648f5515a559
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-30 21:47:01 +0800

    Moved these files as part of the folder restructuring.

---
+++ src/components/Form/Login/loginStyles.js
@@ -0,0 +1,33 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      width: '40ch',
+    }
+  },
+
+  form: {
+    padding: '2em',
+    width: '50ch',
+    margin: 'auto',
+  },
+
+  btn: {
+    width: '50%',
+    marginLeft: '25%',
+    marginRight: '25%',
+    marginTop: '2em',
+  },
+
+  spinner: {
+    marginLeft: '50%',
+    marginRight: '50%',
+  },
+}));
+
+export default useStyles;
