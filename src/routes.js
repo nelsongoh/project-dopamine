67d45dbf9dd1dcc06fd093f0b614559c49c2fe7c
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-01-31 15:19:48 +0800

    Created a routes file to store the route constants.

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+// The key in the object is the permission, while the value maps to the route
+const routes = {
+  public: {
+    index: "/",
+  },
+  protected: {
+    0: {
+      title: "Calendar",
+      url: "/dashboard/calendar",
+    },
+    1: {
+      title: "Profile",
+      url: "/dashboard/profile",
+    },
+    2: {
+      title: "Instagram",
+      url: "/dashboard/ig",
+    },
+  },
+};
+
+Object.freeze(routes);
+
+export default routes;
