cbee71c13ec8f7b6f13fa920b25b2df1b41aed89
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-02 23:46:05 +0800

    Created the permissions widget to be used as part of the create a user form in the admin panel. Can be re-used later when managing a user's permissions.

---
+++ src/components/Form/Widgets/Permissions/permissionsStyles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  perms: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    float: 'right',
+    position: 'relative',
+    padding: theme.spacing(3),
+  },
+}));
+
+export default useStyles;
