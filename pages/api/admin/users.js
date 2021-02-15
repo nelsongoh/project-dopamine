1c3bf8c1e381ce717068b0cbde71826865632fef
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-15 19:28:59 +0800

    Changed the references from the user's UID to the user token for the handler. Also made some minor changes to the HTTP response statuses, and wrote the case for PATCH calls.

--- pages/api/admin/users.js
+++ pages/api/admin/users.js
@@ -1,26 +1,39 @@
 import verifyIsAdmin from '../../../lib/server/verifyIsAdmin';
 import createUser from '../../../lib/server/users/create';
+import updateUser from '../../../lib/server/users/update';
 
 const handler = async (req, res) => {
   const {
-    query: { uid },
+    query: { userToken },
     method,
     body,
   } = req;
 
-  if (uid || body.data.uid) {
-    const isUserAdmin = await verifyIsAdmin(uid || body.data.uid);
+  if (userToken || body.data.userToken) {
+    const isUserAdmin = await verifyIsAdmin(userToken || body.data.userToken);
 
     if (isUserAdmin) {
       switch (method) {
-        case "POST":
+        case "POST": {
           const outcome = await createUser(body.data);
           if (outcome.success) {
-            res.status(200).json({});
+            res.status(204).json({});
           } else {
             res.status(400).json(outcome.errors);
           }
           break;
+        }
+
+        case "PATCH": {
+          const outcome = await updateUser(body.data);
+          if (outcome.success) {
+            res.status(204).json({});
+          } else {
+            console.log(outcome.errors);
+            res.status(400).json(outcome.errors)
+          }
+          break;
+        }
 
         default:
           res.status(404).json({});
