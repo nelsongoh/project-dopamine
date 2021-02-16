02186e9d94116df37c2640305552f688fe38b23a
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-16 14:35:35 +0800

    Included an isOpen prop for the LoadingScreen component.

--- src/components/Loading/loading.js
+++ src/components/Loading/loading.js
@@ -2,9 +2,9 @@ import styles from './loading.module.css';
 import Dialog from '@material-ui/core/Dialog';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const LoadingScreen = () => {
+const LoadingScreen = ({ isOpen }) => {
   return (
-    <Dialog open>
+    <Dialog open={isOpen}>
       <CircularProgress className={styles.spinner} />
       <p className={styles.centerText}>Loading</p>
     </Dialog>
