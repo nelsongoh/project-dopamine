9d684990fe93b17679197acb6dd7b3f6fc82c7cf
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-16 14:43:27 +0800

    The Sidebar component now uses the permissions and URLs obtained from the API instead of using hardcoded routes.

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,54 +1,70 @@
-import { Fragment } from 'react';
+import { Fragment, useContext, useState } from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import LoginContext from '../../contexts/login';
+import PermissionsContext from '../../contexts/permissions';
 import useStyles from './sideBarStyles';
 import Link from '../Link';
-import routes from '../../routes';
+import LoadingScreen from '../Loading/loading';
+import Content from '../../lang';
 
-const Sidebar = ({ content, type, open, onClose, container }) => {
+const Sidebar = ({ type, open, onClose, container }) => {
   const classes = useStyles();
-
+  const { isAdmin } = useContext(LoginContext);
+  const { permissions } = useContext(PermissionsContext);
+  
   return (
-    <Drawer
-      className={classes.drawer}
-      variant={type === "mobile" ? "temporary" : "permanent"}
-      open={type === "mobile" ? open : true}
-      onClose={type === "mobile" ? onClose : null}
-      classes={{ paper: classes.drawerPaper }}
-      container={container}
-      ModalProps={{
-        keepMounted: type === "mobile" ? true : false,
-      }}
-    >
-      <Toolbar />
-        <div className={classes.drawerContainer}>
-          <List>
-            {content.map((permission) => (
-              routes.protected[permission].sublinks.length > 0 ?
-                (
+    <Fragment>
+      {
+        (permissions === null || permissions.length === 0) ? (
+          <LoadingScreen isOpen={true} />
+        ) : (
+          <Drawer
+            className={classes.drawer}
+            variant={type === "mobile" ? "temporary" : "permanent"}
+            open={type === "mobile" ? open : true}
+            onClose={type === "mobile" ? onClose : null}
+            classes={{ paper: classes.drawerPaper }}
+            container={container}
+            ModalProps={{
+              keepMounted: type === "mobile" ? true : false,
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                {permissions.filter((perm) => perm.type !== "admin")
+                  .map((permDetails) => (
+                    <ListItem button component={Link} href={`${permDetails.url}`} key={Math.random()}>
+                      <ListItemText primary={permDetails.title} />
+                    </ListItem>
+                  ))
+                }
+                {isAdmin ? (
                   <Fragment key={Math.random()}>
                     <ListItem>
-                      <ListItemText primary={routes.protected[permission].title}/>
+                      <ListItemText primary={Content('en').sidebar.admin.title}/>
                     </ListItem>
-                    {(routes.protected[permission].sublinks).map((sublink) => (
-                      <ListItem button component={Link} href={`${sublink.url}`} key={Math.random()} className={classes.subLink}>
-                        <ListItemText primary={sublink.title} />
-                      </ListItem>
-                    ))}
+                    {permissions.filter((perm) => perm.type === "admin")
+                      .map((permDetails) => (
+                        <ListItem button component={Link} href={permDetails.url} key={Math.random()} className={classes.subLink}>
+                          <ListItemText primary={permDetails.title} />
+                        </ListItem>
+                      ))}
                   </Fragment>
                 ) : (
-                  <ListItem button component={Link} href={`${routes.protected[permission].url}`} key={Math.random()}>
-                    <ListItemText primary={routes.protected[permission].title} />
-                  </ListItem>
-                )
-            ))}
-          </List>
-        </div>
-    </Drawer>
-  )
+                  null
+                )}
+              </List>
+            </div>
+          </Drawer>
+        )
+      }
+    </Fragment>
+  );
 };
 
 export default Sidebar;
