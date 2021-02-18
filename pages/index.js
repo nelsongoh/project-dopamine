042f73b614cdd783c2ee94c5390a73abb48e6a1c
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-18 19:50:14 +0800

    Made changes to the index page to remove the placeholder Next.js content, with our own. Currently included an image as a background on the index page which dynamically resizes based on the viewport width and height. Mobile-friendly and desktop-friendly.

--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,47 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+import Toolbar from '@material-ui/core/Toolbar';
+import useTheme from '@material-ui/styles/useTheme';
 import Headerbar from '../src/components/Headerbar';
+import { retrieveIndexPageAssets } from '../lib/server/assets';
 
-export default function Home() {
+export const getStaticProps = async (context) => {
+  const { backgroundImg } = await retrieveIndexPageAssets();
+  return {
+    props: {
+      backgroundImg
+    }
+  }
+};
+
+const Home = ({ backgroundImg }) => {
+  const [headerBarHeight, setHeaderBarHeight] = useState(0);
+
+  useEffect(() => {
+    setHeaderBarHeight(document.getElementById("homeToolbar").clientHeight);
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Hello!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <Headerbar />
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!!!!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <div style={{ display: 'flex' }}>
+      <Headerbar isTopStack={true} />
+      <div>
+        <Toolbar id="homeToolbar" />
+        <div style={{ 
+          position: 'absolute', 
+          width: '100vw', 
+          height: `calc(100vh - ${headerBarHeight}px`
+        }}>
+          <Image
+            src={backgroundImg}
+            alt="Landing page image"
+            aria-label="hot-air balloons in the desert"
+            layout="fill"
+            objectFit="cover"
+          />
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      </div>
     </div>
-  )
-}
+  );  
+};
+
+export default Home;
