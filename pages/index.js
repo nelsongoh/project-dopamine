595838df35b959210edd1871b44841e7508c4853
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-19 12:44:47 +0800

    Included responsive styles to adjust the index page's title for different viewport widths. Also moved the styles into a separate function.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import Image from 'next/image';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import useTheme from '@material-ui/styles/useTheme';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { makeStyles } from '@material-ui/core/styles';
 import Headerbar from '../src/components/Headerbar';
 import { retrieveIndexPageAssets } from '../lib/server/assets';
 
@@ -14,33 +17,84 @@ export const getStaticProps = async (context) => {
   }
 };
 
+const useStyles = makeStyles((theme) => ({
+  pageTitle: {
+    position: 'absolute',
+    zIndex: 1,
+    color: 'white',
+    [theme.breakpoints.only("xs")]: {
+      top: '250%',
+      left: '120%',
+      width: '200px',
+    },
+    [theme.breakpoints.only("sm")]: {
+      top: '340%',
+      left: '200%',
+      width: '200px',
+    },
+    [theme.breakpoints.only("md")]: {
+      top: '420%',
+      left: '200%',
+      width: '300px' 
+    },
+    [theme.breakpoints.up("lg")]: {
+      top: '220%',
+      left: '500%',
+      width: '300px' 
+    },
+  },
+}));
+
 const Home = ({ backgroundImg }) => {
   const [headerBarHeight, setHeaderBarHeight] = useState(0);
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const isXtraSmall = useMediaQuery(theme.breakpoints.only("xs"));
+  const isMidAndAbove = useMediaQuery(theme.breakpoints.up("md"));
+
+  const getTypoVariant = () => {
+    if (isXtraSmall) {
+      return "h4";
+    } else if (isMidAndAbove) {
+      return "h1";
+    } else {
+      return "h2";
+    }
+  };
 
   useEffect(() => {
     setHeaderBarHeight(document.getElementById("homeToolbar").clientHeight);
-  }, [])
+  }, []);
 
   return (
-    <div style={{ display: 'flex' }}>
-      <Headerbar isTopStack={true} />
-      <div>
-        <Toolbar id="homeToolbar" />
-        <div style={{ 
-          position: 'absolute', 
-          width: '100vw', 
-          height: `calc(100vh - ${headerBarHeight}px`
-        }}>
-          <Image
-            src={backgroundImg}
-            alt="Landing page image"
-            aria-label="hot-air balloons in the desert"
-            layout="fill"
-            objectFit="cover"
-          />
+    <Fragment>
+      <div style={{ display: 'flex' }}>
+        <Headerbar isTopStack={true} />
+        <div style={{ position: 'relative '}}>
+          <Toolbar id="homeToolbar" />
+          <div className={classes.pageTitle}>
+            <Typography variant={getTypoVariant()} style={{ fontFamily: 'Permanent Marker, cursive' }}>
+              Welcome to the Playground
+            </Typography>
+          </div>
+          <div style={{ 
+            position: 'absolute', 
+            width: '100vw', 
+            height: `calc(100vh - ${headerBarHeight}px`,
+            zIndex: -1,
+          }}>
+            <Image
+              src={backgroundImg}
+              alt="Landing page image"
+              aria-label="hot-air balloons in the desert"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );  
 };
 
