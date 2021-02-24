b28f3481e3725dd44b8bed7bc437950941b66a52
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-24 17:12:10 +0800

    Added the styles for the title text for the FragranceFunction sub-component.

--- src/components/Fragrances/FragranceFunction/fragranceFunctionStyles.js
+++ src/components/Fragrances/FragranceFunction/fragranceFunctionStyles.js
@@ -1,9 +1,13 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+  title: {
+    textAlign: "center",
+    marginBottom: theme.spacing(2),
+  },
   chip: {
     margin: theme.spacing(1),
-  }
+  },
 }));
 
 export default useStyles;
