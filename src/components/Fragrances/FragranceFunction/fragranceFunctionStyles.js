2b90795e11453b5e4794c94adfc56c811ee06112
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-24 16:24:00 +0800

    Added the styles for the FragranceFunction sub-component.

---
+++ src/components/Fragrances/FragranceFunction/fragranceFunctionStyles.js
@@ -0,0 +1,9 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  chip: {
+    margin: theme.spacing(1),
+  }
+}));
+
+export default useStyles;
