0871e5b4ba97679bf9e495a9194ee02af7c39d17
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-02-24 16:19:45 +0800

    Added content relevant for the Fragrances components and sub-components. In particular, the content for each step in the Stepper.

--- src/lang/en.js
+++ src/lang/en.js
@@ -35,6 +35,33 @@ const Content = {
     },
   },
   pages: {
+    fragrances: {
+      steps: [
+        "Choose container size",
+        "Select dilution level",
+        "Decide fragrance function(s)",
+        "Pick ingredients",
+        "Adjust note proportions",
+        "Distribute droplet counts"
+      ],
+      stepDesc: [
+        "Choose a container volume which you would like to use for your fragrance. Alternatively, enter your custom volume.",
+        "Select a dilution level for your fragrance.",
+        "Decide the type of functions you would like for your fragrance.\nNOTE: Your selection here will determine the ingredients available in the next step.",
+        "Pick the ingredients you would like to use for each of the Top, Middle, and Base notes.",
+        "Adjust the volumetric proportion of your Top, Middle, and Base notes. The default proportion has been assigned as 30% (Top), 40% (Middle), and 30% (Base).\nNOTE: The proportion selected here will determine the available droplets in each note type in the next step.",
+        "Distribute the droplet counts for each ingredient within each note type."
+      ],
+      stepDescUnknown: "Unknown step occurred. No known description found.",
+      containerSizes: {
+        ariaLabel: "Container size selection",
+        name: "ContainerSizeSelection",
+      },
+      dilutionLevels: {
+        ariaLabel: "Dilution level selection",
+        name: "DilutionLevelSelection",
+      },
+    },
     admin: {
       users: {
         tabs: {
