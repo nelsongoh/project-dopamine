368ba4e98a5e0a67053f50f7a433bc70ef29e0de
dohpahmine <61051561+nelsongoh@users.noreply.github.com>
2021-03-05 10:05:49 +0800

    Removed the title style from the FragranceFunction sub-component.

--- src/components/Fragrances/FragranceFunction/fragranceFunctionStyles.js
+++ src/components/Fragrances/FragranceFunction/fragranceFunctionStyles.js
@@ -1,10 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  title: {
-    textAlign: "center",
-    marginBottom: theme.spacing(2),
-  },
   chip: {
     margin: theme.spacing(1),
   },
